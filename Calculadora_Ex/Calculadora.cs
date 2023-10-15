aee4ba66414f401293cf8f4d229db5119216edf7
RafaAbreu04 <131555787+RafaAbreu04@users.noreply.github.com>
2023-10-15 16:01:29 -0300

    Arquivos do projeto.

---
+++ Calculadora_Ex/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Ex
+{
+    public static class Calculadora
+    {
+        public static int Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+
+        public static int Subtrair(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }
+
+        public static int Multiplicar(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }
+
+        public static int Dividir(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            return resultado;
+        }
+    }
+}
---
+++ Calculadora_Ex/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Ex
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora\n");
+
+            while()
+            Console.Write("Insira o primeiro número da operação: ");
+
+            string strNum1 = Console.ReadLine();
+            if (!int.TryParse(strNum1, out int num1))
+            {
+                Console.WriteLine("Número inválido, insira um número inteiro!");
+                //while pra caso a pessoa erre
+                Environment.Exit(-1);
+            }
+
+            Console.Write("Insira o segundo número da operação: \n");
+            string strNum2 = Console.ReadLine();
+            if (!int.TryParse(strNum2, out int num2))
+            {
+                Console.WriteLine("Número inválido, insira um número inteiro!");
+                //while pra caso a pessoa erre
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine("Escolha a operação que deseja realizar: \n");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+
+            string opcao = Console.ReadLine();
+            opcao.ToLower();
+            if (opcao.Equals("1") || opcao.Equals("soma") || opcao.Equals("+"))             
+            {
+                Console.WriteLine($"Resultado: {Calculadora.Somar(num1, num2)}");
+            }
+            else if (opcao.Equals("2") || opcao.Equals("subtrair") || opcao.Equals("-"))
+            {
+                Console.WriteLine($"Resultado: {Calculadora.Subtrair(num1, num2)}");
+            }
+            else if (opcao.Equals("3") || opcao.Equals("multiplicar") || opcao.Equals("*"))
+            {
+                Console.WriteLine($"Resultado: {Calculadora.Multiplicar(num1, num2)}");
+            }
+            else if (opcao.Equals("4") || opcao.Equals("dividir") || opcao.Equals("/"))
+            {
+                Console.WriteLine($"Resultado: {Calculadora.Dividir(num1, num2)}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
