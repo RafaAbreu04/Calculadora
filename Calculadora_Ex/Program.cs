0b6ea098b896c1ba74aab5206bf7b2832c965a6a
Rafaelly Abreu <rafaellyabreu84@gmail.com>
2023-10-15 17:32:21 -0300

    Calculadora com loop

--- Calculadora_Ex/Program.cs
+++ Calculadora_Ex/Program.cs
@@ -10,55 +10,73 @@ namespace Calculadora_Ex
     {
         public static void Main(string[] args)
         {
+            bool repetirCalculos = true;
+
             Console.WriteLine("Calculadora\n");
 
-            while()
-            Console.Write("Insira o primeiro número da operação: ");
-
-            string strNum1 = Console.ReadLine();
-            if (!int.TryParse(strNum1, out int num1))
+            while (repetirCalculos)
             {
-                Console.WriteLine("Número inválido, insira um número inteiro!");
-                //while pra caso a pessoa erre
-                Environment.Exit(-1);
-            }
+                Console.Write("Insira o primeiro número da operação: ");
 
-            Console.Write("Insira o segundo número da operação: \n");
-            string strNum2 = Console.ReadLine();
-            if (!int.TryParse(strNum2, out int num2))
-            {
-                Console.WriteLine("Número inválido, insira um número inteiro!");
-                //while pra caso a pessoa erre
-                Environment.Exit(-1);
-            }
+                string strNum1 = Console.ReadLine();
+                if (!int.TryParse(strNum1, out int num1))
+                {
+                    Console.WriteLine("Número inválido!");                    
+                    Environment.Exit(-1);
+                }
 
-            Console.WriteLine("Escolha a operação que deseja realizar: \n");
-            Console.WriteLine("1 - Soma");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("3 - Multiplicar");
-            Console.WriteLine("4 - Dividir");
+                Console.Write("Insira o segundo número da operação: ");
 
-            string opcao = Console.ReadLine();
-            opcao.ToLower();
-            if (opcao.Equals("1") || opcao.Equals("soma") || opcao.Equals("+"))             
-            {
-                Console.WriteLine($"Resultado: {Calculadora.Somar(num1, num2)}");
-            }
-            else if (opcao.Equals("2") || opcao.Equals("subtrair") || opcao.Equals("-"))
-            {
-                Console.WriteLine($"Resultado: {Calculadora.Subtrair(num1, num2)}");
-            }
-            else if (opcao.Equals("3") || opcao.Equals("multiplicar") || opcao.Equals("*"))
-            {
-                Console.WriteLine($"Resultado: {Calculadora.Multiplicar(num1, num2)}");
-            }
-            else if (opcao.Equals("4") || opcao.Equals("dividir") || opcao.Equals("/"))
-            {
-                Console.WriteLine($"Resultado: {Calculadora.Dividir(num1, num2)}");
-            }
+                string strNum2 = Console.ReadLine();
+                if (!int.TryParse(strNum2, out int num2))
+                {
+                    Console.WriteLine("Número inválido!");
+                    Environment.Exit(-1);
+                }
 
+                Console.WriteLine("\nEscolha a operação que deseja realizar: \n");
+                Console.WriteLine("1 - Soma");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicar");
+                Console.WriteLine("4 - Dividir");
 
-            Console.ReadLine();
+                string opcao = Console.ReadLine().ToLower();
+
+                if (opcao.Equals("1") || opcao.Equals("soma") || opcao.Equals("+"))             
+                {
+                    Console.WriteLine($"Resultado: {Calculadora.Somar(num1, num2)}");
+                }
+                else if (opcao.Equals("2") || opcao.Equals("subtrair") || opcao.Equals("-"))
+                {
+                    Console.WriteLine($"Resultado: {Calculadora.Subtrair(num1, num2)}");
+                }
+                else if (opcao.Equals("3") || opcao.Equals("multiplicar") || opcao.Equals("*"))
+                {
+                    Console.WriteLine($"Resultado: {Calculadora.Multiplicar(num1, num2)}");
+                }
+                else if (opcao.Equals("4") || opcao.Equals("dividir") || opcao.Equals("/"))
+                {
+                    Console.WriteLine($"Resultado: {Calculadora.Dividir(num1, num2)}");
+                }
+
+                Console.WriteLine("Deseja calcular algo novamente?\n");
+                Console.WriteLine("1 - Sim.");
+                Console.WriteLine("2 - Não.");
+
+                string resposta = Console.ReadLine().ToLower();
+                if(resposta.Equals("2") || resposta.Equals("não") || resposta.Equals("n"))
+                {
+                    repetirCalculos = false;
+                }
+                else if(resposta.Equals("1") || resposta.Equals("sim") || resposta.Equals("s"))
+                {
+                    repetirCalculos = true;
+                }
+                
+                Console.ReadLine();
+
+            }
+            
         }
     }
 }
