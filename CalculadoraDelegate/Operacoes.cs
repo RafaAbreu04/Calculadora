1563ce11ffab5c4cc1c463e35dba21b3f629350e
RafaAbreu04 <131555787+RafaAbreu04@users.noreply.github.com>
2025-04-06 17:46:41 -0300

    Calculadora com usando delegate
    Adicionei uma versão que fiz utilizando delegate.

---
+++ CalculadoraDelegate/Operacoes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDelegate
+{
+    class Operacoes
+    {
+        public static int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Subtracao(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public static int Multiplicacao(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public static int Divisao(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
---
+++ CalculadoraDelegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDelegate
+{
+    class Program
+    {
+        public delegate int Funcao(int n1, int n2);
+
+        static void Main(string[] args)
+        {
+            Funcao funcao = new Funcao(Operacoes.Soma);
+            int resultado = funcao(1, 3);
+
+            decimal x = 7 / 5;
+            Console.WriteLine(x);
+
+
+            Console.WriteLine($"Soma: {resultado}");
+
+            funcao = new Funcao(Operacoes.Subtracao);
+            resultado = funcao(2, 1);
+
+            Console.WriteLine($"Subtração: {resultado}");
+
+            funcao = new Funcao(Operacoes.Divisao);
+            resultado = funcao(10, 5);
+
+            Console.WriteLine($"Divisão: {resultado}");
+
+            funcao = new Funcao(Operacoes.Multiplicacao);
+            resultado = funcao(8, 8);
+
+            Console.WriteLine($"Multiplicação: {resultado}");
+
+            Console.ReadLine();
+        }
+    }
+}
